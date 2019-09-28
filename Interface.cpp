44d048901b487f9362bd9f950725714670d7936d
Henry <lefevrehenry@hotmail.fr>
2019-09-29 01:19:59 +0200

    add Camera and SceneView property to Interface

--- Interface.cpp
+++ Interface.cpp
@@ -1,6 +1,37 @@
 #include "Interface.h"
 
-gl::Interface::~Interface()
+#include "Camera.h"
+#include "SceneView.h"
+
+using namespace gl;
+
+Controller::~Controller()
 {
 
+}
+
+DefaultController::DefaultController(std::weak_ptr<SceneView> sceneView) :
+    m_sceneView(sceneView)
+{
+
+}
+
+SceneView* DefaultController::sceneView() const
+{
+    std::shared_ptr<SceneView> sceneView(this->m_sceneView);
+
+    if(sceneView)
+        return sceneView.get();
+
+    return nullptr;
+}
+
+Camera* DefaultController::camera() const
+{
+    SceneView* sceneView = DefaultController::sceneView();
+
+    if(sceneView)
+        return sceneView->camera();
+
+    return  nullptr;
 }
--- Interface.h
+++ Interface.h
@@ -1,50 +1,57 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+// Standard Library
+#include <memory>
+
 
 class GLFWwindow;
 
 namespace gl {
 
+class Camera;
+class SceneView;
+
 /**
- * @brief The Interface class
+ * @brief The Controller class
  */
-class Interface
+class Controller
 {
 
 public:
+    virtual ~Controller();
 
-    virtual ~Interface();
-
+public:
     virtual void framebufferSizeCallback(GLFWwindow* handle, int width, int height) = 0;
-
     virtual void mouseButtonCallback(GLFWwindow* handle, int button, int action, int mods) = 0;
-
     virtual void cursorPosCallback(GLFWwindow* handle, double xpos, double ypos) = 0;
-
     virtual void scrollCallback(GLFWwindow* handle, double xoffset, double yoffset) = 0;
-
     virtual void keyCallback(GLFWwindow* handle, int key, int scancode, int action, int mods) = 0;
 
 };
 
 /**
- * @brief The DefaultInterface class
+ * @brief The DefaultCntroller class
  */
-class DefaultInterface : public Interface
+class DefaultController : public Controller
 {
 
 public:
+    DefaultController(std::weak_ptr<SceneView> sceneView);
 
+public:
     virtual void framebufferSizeCallback(GLFWwindow*, int, int) {}
-
     virtual void mouseButtonCallback(GLFWwindow*, int, int, int) {}
-
     virtual void cursorPosCallback(GLFWwindow*, double, double) {}
-
     virtual void scrollCallback(GLFWwindow*, double, double) {}
-
     virtual void keyCallback(GLFWwindow*, int, int, int, int) {}
+
+public:
+    SceneView* sceneView() const;
+    Camera* camera() const;
+
+private:
+    std::weak_ptr<SceneView> m_sceneView;
 
 };
 
