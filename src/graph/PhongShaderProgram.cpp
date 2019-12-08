c66f3aef2367ffd4eff44c88cdbb6b6e5fbb3801
Henry <lefevrehenry@hotmail.fr>
2019-12-09 00:34:48 +0100

    typo

--- src/graph/PhongShaderProgram.cpp
+++ src/graph/PhongShaderProgram.cpp
@@ -1,12 +1,10 @@
 #include "PhongShaderProgram.h"
 
 #include <gltk.h>
-#include <ShaderProgramPrivate.h>
-#include <VisualManager.h>
 
 using namespace gl;
 
-PhongShaderProgram::PhongShaderProgram()
+PhongShaderProgram::PhongShaderProgram() : ShaderProgram()
 {
     std::string vertexCode = GLTK::GetStringFromFile("shaders/phongShading.vs");
     std::string fragmentCode = GLTK::GetStringFromFile("shaders/phongShading.fs");
